e920951a69e73cb5a61633a2f0f1de15a2493b12
sonedayan@g <sonedayan@gamil.com>
2022-11-22 18:02:49 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const phoneNumberValue = document.getElementById('phone_id');
+let carrierValue = document.getElementById('carrier_id');
+
+phoneNumberValue.addEventListener('onChange', ()=>{
+    if (condition) {
+        
+    }
+   carrierValue.value = phoneNumberValue.value;
+});
