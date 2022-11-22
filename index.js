2180bcb3282da350722762426f3b1a0038ee2f57
sonedayan <67210349+sonedayan@users.noreply.github.com>
2022-11-22 17:56:28 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const phoneNumberValue = document.getElementById('phone_id');
-let carrierValue = document.getElementById('carrier_id');
-
-phoneNumberValue.addEventListener('onChange', ()=>{
-    if (condition) {
-        
-    }
-   carrierValue.value = phoneNumberValue.value;
-});
